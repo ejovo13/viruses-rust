ad05de27a7d76b27d575c199a306a0ed0f72dfbd
Evan Voyles linux-laptop <ejovo@yahoo.com>
2021-08-02 00:27:55 -0400

    add modules

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ extern crate async_std;
 
 mod vdb_download;
 mod cli;
+mod math;
+mod mol;
+mod vdb_reader;
 
 // use error_chain::error_chain;
 use vdb_download as vdb;
@@ -11,6 +14,8 @@ use vdb_download as vdb;
 
 #[tokio::main]
 async fn main() -> Result<(), ()> {
+
+    math::print_vec();
 
     cli::run();
     vdb::do_everything().await;
