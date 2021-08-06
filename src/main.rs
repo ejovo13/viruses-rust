a711400ca281a59b6deeca50e44d6011e0bd19e9
Evan Voyles linux-laptop <ejovo@yahoo.com>
2021-08-06 17:49:16 -0400

    Refactor vdb into its own parent module

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 extern crate async_std;
 
-mod vdb_download;
 mod cli;
 mod math;
 mod mol;
-mod vdb_reader;
-
+mod vdb;
 // use error_chain::error_chain;
-use vdb_download as vdb;
 
 
 
@@ -18,9 +15,8 @@ async fn main() -> Result<(), ()> {
     math::print_vec();
 
     cli::run();
-    let vdb_pair = vdb::do_everything().await;
-    let vdb = vdb_reader::do_everything(vdb_pair);
-
+    let vdb_pair = vdb::downloader::do_everything().await;
+    let vdb = vdb::reader::do_everything(vdb_pair);
 
     Ok(())
 }
