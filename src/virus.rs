a4df79a734ca268ec31857b271586737d48c2f56
Evan Voyles linux-laptop <ejovo@yahoo.com>
2021-08-06 22:47:20 -0400

    Make virus class

--- src/virus.rs
+++ src/virus.rs
@@ -1,33 +1,71 @@
+use crate::vdb::{VDB, VDBInfo, Atom};
+use std::fmt::{Display, Formatter};
+
 // Create a virus structure that stores information like where the pdb is stored on the current system
 // Also tries to gather basic information about the virus
-struct Virus {
+pub struct Virus {
 
-
-
-
-
-
+    name: String,
+    pdbid: String,
+    au: bool,
+    atoms: Vec<Atom>,
+    genus: String,
+    genome: String,
+    host: String,
+    t_number: usize,
+    n_atoms: usize,
+    resolution: f64,
+    // TODO Add atoms per protein
 
 }
 
-/* Method that converts a vdb to a complete virus object.
 
-    In order to do this, we have to fill in the following information:
+impl Virus {
 
-    - Name
-    - T number
-    - family
-    - genus
-    - atoms per protein
-    - everything!!
+    pub fn from(vdb: VDB, info: VDBInfo) -> Self {
 
-    I want two different interfaces. One should access the backend json and one should hunt through the html.
-    This is a good way to learn.
+        Virus {
 
-*/
+            name: info.name,
+            pdbid: vdb.pdbid,
+            au: vdb.au,
+            atoms: vdb.atoms,
+            genus: info.genus,
+            genome: info.genome,
+            host: info.host,
+            t_number: info.t_number,
+            resolution: info.resolution,
+            n_atoms: vdb.num_atoms,
+        }
+    }
 
-fn query_information(pdbid: &str) {
+    fn max_radius(&self) -> f64 {
 
+        let mut max_r = 0.;
+        for a in self.atoms.iter() {
+            if a.radius() > max_r {
+                max_r = a.radius();
+            }
+        }
 
+        max_r
+    }
+}
 
+impl Display for Virus {
+
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+
+        writeln!(f, "{}", self.name);
+        writeln!(f, "Pdbid:             {}", self.pdbid);
+        writeln!(f, "T-number:          {}", self.t_number);
+        writeln!(f, "N_atoms:           {}", self.n_atoms);
+        writeln!(f, "au:                {}", self.au);
+        writeln!(f, "genus:             {}", self.genus);
+        writeln!(f, "genome:            {}", self.genome);
+        writeln!(f, "host:              {}", self.host);
+        writeln!(f, "resolution:        {}", self.resolution);
+        writeln!(f, "max_radius:        {}", self.max_radius());
+        Ok(())
+    }
 }
